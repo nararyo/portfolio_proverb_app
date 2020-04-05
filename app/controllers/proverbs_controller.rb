042b2fdbd3f23f0b379736cebbbd602e2e9e8e26
奈良亮 <motorbicykle@gmail.com>
2020-04-05 22:25:03 +0900

    ことわざと作者をビューでランダムに表示

--- app/controllers/proverbs_controller.rb
+++ app/controllers/proverbs_controller.rb
@@ -3,6 +3,42 @@ class ProverbsController < ApplicationController
   end
 
   def show
-    
   end
+
+  def anxious
+    proverbs_type = Proverb.where(feel:"anxious").find_each.pluck(:who_said, :expression)
+    proverb = proverbs_type.sample
+    @expression  = proverb[1]
+    @proverbed_by = proverb[0]
+
+  end
+
+  def depress
+    proverbs_type = Proverb.where(feel:"anxious").find_each.pluck(:who_said, :expression)
+    proverb = proverbs_type.sample
+    @expression  = proverb[1]
+    @proverbed_by = proverb[0]
+  end
+
+  def sad
+    proverbs_type = Proverb.where(feel:"anxious").find_each.pluck(:who_said, :expression)
+    proverb = proverbs_type.sample
+    @expression  = proverb[1]
+    @proverbed_by = proverb[0]
+  end
+
+  def tired
+    proverbs_type = Proverb.where(feel:"anxious").find_each.pluck(:who_said, :expression)
+    proverb = proverbs_type.sample
+    @expression  = proverb[1]
+    @proverbed_by = proverb[0]
+  end
+
+  def up
+    proverbs_type = Proverb.where(feel:"anxious").find_each.pluck(:who_said, :expression)
+    proverb = proverbs_type.sample
+    @expression  = proverb[1]
+    @proverbed_by = proverb[0]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "proverbs#index"
-  get 'proverbs/show'
+  get "proverbs/anxious", to: "proverbs#anxious"
+  get "proverbs/depress", to: "proverbs#depress"
+  get "proverbs/down", to: "proverbs#down"
+  get "proverbs/sad", to: "proverbs#sad"
+  get "proverbs/tired", to: "proverbs#tired"
+  get "proverbs/up", to: "proverbs#up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
